1c0e1b4b4f9fc39b70d8d636c4aae8e7d8ae6537
dhykdika <dikarizky66@gmail.com>
2025-03-05 13:55:17 +0700

    eucledian 1 dimension array

---
+++ PHPdistance.php
@@ -0,0 +1,29 @@
+<?php
+
+class DistanceMeasurement {
+
+    //function for eucledian distance with vector 1 dimension.
+    public function eucledianDistance($vectorA, $vectorB) {
+
+        //check length vector first:
+        if(sizeof($vectorA)==sizeof($vectorB)){
+
+            $sigma=0;
+            for($i=0; $i<sizeof($vectorA); $i++){
+                $sigma+=($vectorA[$i]-$vectorB[$i])*($vectorA[$i]-$vectorB[$i]);
+            }
+                $result=sqrt($sigma);
+
+            return $result;
+        }else{
+            return "The length of Array is different";
+        }
+
+    }
+}
+
+    $distance= new DistanceMeasurement();
+    $result=$distance->eucledianDistance([1,1],[2,2]);
+    echo $result;
+
+?>
